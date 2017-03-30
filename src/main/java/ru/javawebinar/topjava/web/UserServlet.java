874f7d8f5148cafa6e11b19434f3703d5c16af25
Александр <alexandr.sidorkin@gmail.com>
2017-03-31 01:22:02 +0300

    logging replicated

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,17 +1,23 @@
 package ru.javawebinar.topjava.web;
 
+import org.slf4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 /**
  * Created by Александр on 2017-03-31.
  */
 public class UserServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(UserServlet.class);
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LOG.debug("redirrect to userList");
 //        req.getRequestDispatcher("/userList.jsp").forward(req,resp);
         resp.sendRedirect("userList.jsp");
     }
