4f043704356497d8263fe4ab95d83c0fddb01c51
Александр <alexandr.sidorkin@gmail.com>
2017-04-04 01:52:32 +0300

    classes created

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,26 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Created by Александр on 2017-04-04.
+ */
+public class MealServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(UserServlet.class);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirrect to userList");
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
